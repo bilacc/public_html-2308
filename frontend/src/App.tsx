63b570a6d2e2ca7244c418a1cb95a78b3d4642b7
Josip Bilać <39159422+bilacc@users.noreply.github.com>
2025-10-17 13:30:17 +0200

    Update frontend/src/App.tsx with React Router routes for PropertyList and PropertyDetail

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,52 +1,16 @@
-// frontend/src/App.tsx
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import PropertyList from './components/PropertyList';
+import PropertyDetail from './components/PropertyDetail';
 
-const App: React.FC = () => {
-  return (
-    <div>
-      <h1>Real Estate Listings</h1>
-      <PropertyList />
-    </div>
-  );
-};
+const App: React.FC = () => (
+  <div>
+    <h1>Real Estate Listings</h1>
+    <Routes>
+      <Route path='/' element={<PropertyList />} />
+      <Route path='/property/:id' element={<PropertyDetail />} />
+    </Routes>
+  </div>
+);
 
 export default App;
-
-// frontend/src/components/PropertyList.tsx
-import React, { useEffect, useState } from 'react';
-
-interface Property {
-  id: number;
-  ime: string;
-  prezime: string;
-  adresa: string;
-  cijena: string;
-}
-
-const PropertyList: React.FC = () => {
-  const [properties, setProperties] = useState<Property[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:4000/api/properties')
-      .then(res => res.json())
-      .then(data => setProperties(data))
-      .catch(console.error);
-  }, []);
-
-  return (
-    <div>
-      <ul>
-        {properties.map(property => (
-          <li key={property.id}>
-            <strong>{property.ime} {property.prezime}</strong><br />
-            Address: {property.adresa}<br />
-            Price: {property.cijena} EUR
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default PropertyList;
